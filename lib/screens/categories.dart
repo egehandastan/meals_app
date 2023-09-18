97d633807943f869eb536e8f472209eee2aafaea
LT-EDASTAN <edastan@limak.com.tr>
2023-09-18 13:23:39 +0300

    data is passed

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
     Navigator.of(context).push(MaterialPageRoute(
-        builder: (ctx) => MealsScreen(title: "some title", meals: [])));
+        builder: (ctx) =>
+            MealsScreen(title: category.title, meals: filteredMeals)));
   }
 
   @override
@@ -27,7 +32,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectCategory: () {
-                _selectCategory(context);
+                _selectCategory(context, category);
               },
             )
         ],
