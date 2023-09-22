b009ab3d66072dcc48b401789447b33f9c785158
Egehan Daştan <87079212+egehandastan@users.noreply.github.com>
2023-09-22 10:31:20 +0300

    metadata is added

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
   final Meal meal;
+  String get complexityText{
+    return meal.complexity.name[0].toUpperCase() + meal.complexity.name.substring(1) ;
+  }
+  String get affordabilityText{
+    return meal.affordability.name[0].toUpperCase() + meal.affordability.name.substring(1) ;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,13 +25,12 @@ class MealItem extends StatelessWidget {
         child: Stack(
           children: [
             FadeInImage(
-                placeholder: MemoryImage(kTransparentImage),
-                image: NetworkImage(meal.imageUrl),
-                  fit: BoxFit.cover,
-                  height: 200,
-                  width: double.infinity,
-                ),
-              
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
+            ),
             Positioned(
                 bottom: 0,
                 right: 0,
@@ -42,16 +48,31 @@ class MealItem extends StatelessWidget {
                         softWrap: true,
                         overflow: TextOverflow.ellipsis,
                         style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white
-                        ),
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
                       ),
                       const SizedBox(
                         height: 12,
                       ),
                       Row(
-                        children: [],
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          MealItemTrait(
+                              icon: Icons.schedule,
+                              label: '${meal.duration} min'
+                              ),
+                              const SizedBox(width: 12,),
+                               MealItemTrait(
+                              icon: Icons.work,
+                              label: complexityText
+                              ),
+                               const SizedBox(width: 12,),
+                               MealItemTrait(
+                              icon: Icons.attach_money,
+                              label: affordabilityText
+                              ),
+                        ],
                       )
                     ],
                   ),
